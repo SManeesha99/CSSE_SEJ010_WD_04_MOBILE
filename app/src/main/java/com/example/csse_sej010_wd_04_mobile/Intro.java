747a75aa420dae01f78b021b67c1c8085c3deed5
SManeesha99 <it20649012@my.sliit.lk>
2022-11-04 11:46:28 +0530

    Revert "Merge branch 'main' of https://github.com/SManeesha99/CSSE_SEJ010_WD_04_MOBILE"
    This reverts commit 2e7510b35991561af0d5dee4a992c7cbe603281c, reversing
    changes made to 5f458e3af36d6e7650e0d181ecd832dd95b6d88d.

--- app/src/main/java/com/example/csse_sej010_wd_04_mobile/Intro.java
+++ app/src/main/java/com/example/csse_sej010_wd_04_mobile/Intro.java
@@ -1,45 +1,14 @@
 package com.example.csse_sej010_wd_04_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.ImageView;
-
-import com.airbnb.lottie.LottieAnimationView;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
 
 public class Intro extends AppCompatActivity {
-
-    Timer timer;
-
-    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        timer =new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(Intro.this,Signin.class);
-                startActivity(intent);
-                finish();
-            }
-        },10000);
-
-
-        lottieAnimationView = findViewById(R.id.lottie);
-
-        lottieAnimationView.animate().setDuration(1000).setStartDelay(10000);
-
     }
 }
